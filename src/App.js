3d12143c548e5fc0bdfb3aa39896986785a63e0c
gayathri <gayathrishriyas@gmail.com>
2024-10-19 16:43:09 +0530

    Temp created

--- src/App.js
+++ src/App.js
@@ -15,8 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Webdevelopment
         </a>
+        <Temp/>
       </header>
     </div>
   );
---
+++ src/Temp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Temp() {
+  return (
+    <div>
+      <h1>This is temp component</h1>
+    </div>
+  )
+}
